b3ca98481ef82089718ffc2261cad6038d76b9a9
developer <143173301+FirstQuantumLabs@users.noreply.github.com>
2023-08-31 15:43:30 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,12 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <div>
+    <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+    // Other layout/components
+  </div>
   )
 }
